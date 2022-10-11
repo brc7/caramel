f61a016b9ab7674de589db33b664d1c030f4d067
David Torres <david@thirdai.com>
2022-10-10 19:12:50 -0500

    merge

--- test/test_codec.py
+++ test/test_codec.py
@@ -1,19 +1,13 @@
 import pytest
-from caramel.Codec import (calculate_frequencies, canonical_huffman,
-                           make_canonical_huffman)
+import numpy as np
+from caramel.Codec import canonical_decode, canonical_huffman
 
-def verify_canonical_huffman(values):
-    # our implementation
-    actual_codedict = make_canonical_huffman(values)
+def verify_canonical_huffman(symbols):
+    codedict, code_length_counts, sorted_symbols = canonical_huffman(symbols)
 
-    # bitarray's implementation
-    frequency_map = calculate_frequencies(values)
-    expected_codedict, counts, symbols = canonical_huffman(frequency_map)
-
-    # can't do == on the maps because two identical bitarrays aren't equal
-    for actual_key, expected_key in zip(sorted(actual_codedict.keys()), sorted(expected_codedict.keys())):
+    for expected_key, code in codedict.items():
+        actual_key = canonical_decode(code, code_length_counts, sorted_symbols)
         assert actual_key == expected_key
-        assert actual_codedict[actual_key].to01(), expected_codedict[expected_key].to01()
 
 
 @pytest.mark.skip(reason="This currently fails")
